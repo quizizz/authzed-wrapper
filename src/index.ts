89fa6efae9370838b1821c26b62f2fd2a9831cf9
Anirudh Singh <anirudh@quizizz.com>
2022-10-18 12:11:37 +0530

    Basic authzed wrapper setup, added some tests, added test and compile actions to publish lib

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const getClient = ({
+  host = '',
+  token = '',
+}: {
+  token: string;
+  host: string;
+}) => {};
---
+++ tests/setup/global-teardown.ts
@@ -0,0 +1,4 @@
+import dockerCompose from 'docker-compose';
+import path from 'path';
+
+export default async function teardown() {}
